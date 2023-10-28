9e13dc6941502ce4fe0bd19218c3ed247a5f68fe
Poddubniy_F_A <21federror12@gmail.ru>
2023-10-28 12:51:44 +0300

    feat: create program project of the solution

---
+++ Programm/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] getEditedArray(string[] array, int maxLength) {
+    int counter = 0;
+    int length = array.Length;
+    int[] indicators = new int[length];
+
+    for (int i = 0; i < length; i++) {
+        if (array[i].Length <= maxLength) {
+            indicators[i] = 1;
+            counter++;
+        }
+    }
+
+    string[] result = new string[counter];
+    for (int i = length - 1; i >= 0; i--) {
+        if (indicators[i] == 1) {
+            counter--;
+            result[counter] = array[i];
+        }
+    }
+    return result;
+}
+
+void PrintArray(string[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write($" {array[i]}");
+    }
+}
+
+string[] numbers = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+
+Console.Write("Измененный массив:");
+PrintArray(getEditedArray(numbers, 3));
